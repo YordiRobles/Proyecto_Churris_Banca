634c2bcf71732d3f7a61272e1a39d29762327c01
Yordi <yordi.robles@ucr.ac.cr>
2024-05-07 11:26:31 -0600

    Modificaciones en las vistas dashboard y search result

--- proyecto_churris_banca/resources/views/search_result.blade.php
+++ proyecto_churris_banca/resources/views/search_result.blade.php
@@ -4,7 +4,7 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="header-container">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <h2 class="font-title">
                 {{ __('Red Social') }}
             </h2>
             <div class="search-container">
