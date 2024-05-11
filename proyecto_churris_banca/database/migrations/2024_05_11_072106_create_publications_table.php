5db916de5ff5b7fb10091ce4272c81cf7eede8a5
gemonk <gemonhk13@gmail.com>
2024-05-11 01:40:23 -0600

    migraciones tablas

---
+++ proyecto_churris_banca/database/migrations/2024_05_11_072106_create_publications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('text', 200);
+            $table->string('image')->nullable();
+            $table->integer('likes_count')->default(0);
+            $table->integer('dislikes_count')->default(0);
+            $table->timestamps();
+
+            // Define foreign key relationship with the 'users' table
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publications');
+    }
+};
