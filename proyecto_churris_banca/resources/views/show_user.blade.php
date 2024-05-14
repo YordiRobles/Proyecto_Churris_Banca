7c403890c5442d2e8f51e5d767300d83e9f63d94
Yordi <yordi.robles@ucr.ac.cr>
2024-05-14 16:56:05 -0600

    Se modifican los botones de seguimiento

--- proyecto_churris_banca/resources/views/show_user.blade.php
+++ proyecto_churris_banca/resources/views/show_user.blade.php
@@ -1,17 +1,11 @@
 <link href="/css/followuser.css" rel="stylesheet">
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Show User</title>
-</head>
+
 <body>
     @if (isset($name))
         <h1>User Name: {{ $name }}</h1>
     @endif
-    <p>asdasdasd </p>
-
     @if (isset($email))
         <p>Email: {{ $email }}</p>
     @endif
@@ -19,11 +13,11 @@
     <form id="followForm" action="{{ route('user.follow', ['name' => $name]) }}" method="POST">
     @csrf
         <div class="follow-buttons">
-            <button type="submit" name="follow" value="1" id="followButton">
-            <span class="button__text">Seguir usuario</span>
+            <button type="submit" name="follow" class="buttonfollow" value="1" id="followButton">
+            <span>Empezar a eguir</span>
             </button>
-            <button type="submit" name="unfollow" value="2" id="unfollowButton">
-            <span class="button__text">Dejar de seguir usuario</span>
+            <button type="submit" class= "buttonunfollow "name="unfollow" value="2" id="unfollowButton">
+            <span>Dejar de seguir</span>
             </button>
         </div>
     </form>
