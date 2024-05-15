2b2e6a7667c4a6cca1ebc680d273c4e3caa0a931
sebastianrodrigueztencio <sebastian.rodrigueztencio@ucr.ac.cr>
2024-05-15 01:37:24 -0600

    Se crea el controlador para la pantalla de dashboard principal y se agregan las respectivas conexiones.

--- proyecto_churris_banca/routes/web.php
+++ proyecto_churris_banca/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,9 +20,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -29,6 +30,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/searchusers', [UserController::class, 'searchUsers'])->name('search.users');
     Route::get('/showuser/{name}', [UserController::class, 'show'])->name('user.show');
+
+    // Dashboard Routes
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/store-post', [DashboardController::class, 'storePost'])->name('store.post');
 });
 
 require __DIR__.'/auth.php';
