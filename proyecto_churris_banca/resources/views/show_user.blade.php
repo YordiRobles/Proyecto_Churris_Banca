e7291cc58d7a99016b47eec0a8634fc6ba5e1cce
YordiRobles <yordi.robles@ucr.ac.cr>
2024-05-15 13:27:24 -0600

    Se arregla la vista donde se sigue un usuario

--- proyecto_churris_banca/resources/views/show_user.blade.php
+++ proyecto_churris_banca/resources/views/show_user.blade.php
@@ -17,32 +17,35 @@
                 </div>
             </div>
         </x-slot>
-        <body>
+        <div class="container">
             @if (isset($name))
-            <div class="title-container">
-                <h2 class="title-result">{{ $name }}</h2>
-             </div>
+                <div class="title-container">
+                    <h2 class="title-username">{{ $name }}</h2>
+                </div>
             @endif
             @if (isset($email))
-                <p>Email: {{ $email }}</p>
-            @endif
-
-            <form id="followForm" action="{{ route('user.follow', ['name' => $name]) }}" method="POST">
-            @csrf
-                <div class="follow-buttons">
-                    <button type="submit" name="follow" class="buttonfollow" value="1" id="followButton">
-                    <span>Empezar a seguir</span>
-                    </button>
-                    <button type="submit" class= "buttonunfollow "name="unfollow" value="2" id="unfollowButton">
-                    <span>Dejar de seguir</span>
-                    </button>
+                <div class="title-container">
+                    <h2 class="title-email">{{ $email }}</h2>
                 </div>
-            </form>
+            @endif
+            <div class ="followform">
+                <form id="followForm" action="{{ route('user.follow', ['name' => $name]) }}" method="POST">
+                    @csrf
+                    <div class="follow-buttons">
+                        <button type="submit" name="follow" class="buttonfollow" value="1" id="followButton">
+                            <span>Empezar a seguir</span>
+                        </button>
+                        <button type="submit" class= "buttonunfollow "name="unfollow" value="2" id="unfollowButton">
+                            <span>Dejar de seguir</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
             <script>
                 var isFollowing = {{ json_encode($is_following) }};
             </script>
             <script src="{{ asset('/js/followbutton.js') }}" defer></script>
-        </body>
     </x-app-layout>
 </html>
